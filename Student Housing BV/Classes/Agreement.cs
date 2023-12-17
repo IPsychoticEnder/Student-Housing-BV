03c6ddd8e1869abe786b269d43606878e09776e9
Aaron Boekhoudt <95099604+IPsychoticEnder@users.noreply.github.com>
2023-12-17 14:44:57 +0100

    accidentally worked on main again-added start to agreements

---
+++ Student Housing BV/Classes/Agreement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Housing_BV.Classes
+{
+    internal class Agreement
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Agreement(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+}
---
+++ Student Housing BV/Classes/HandleAgreements.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Housing_BV.Classes
+{
+    internal class HandleAgreements
+    {
+        public List<Agreement> Agreements { get; private set; }
+        public string FilePath { get; private set; }
+        
+
+        public HandleAgreements() 
+        {
+            Agreements = new List<Agreement>();
+
+            string relativeFilepath = "\\Data\\Json\\Agreements.json";
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            while (!currentDirectory.EndsWith("\\Student Housing BV"))
+            {
+                currentDirectory = Directory.GetParent(currentDirectory).FullName;
+            }
+            this.FilePath = $"{currentDirectory}{relativeFilepath}";
+
+            ReadFromJson();
+
+        }
+
+        public void AddAgreement(Agreement newAgreement)
+        {
+
+        }
+
+        public void ReadFromJson()
+        {
+            try
+            {
+                string jsonContent = File.ReadAllText(FilePath);
+
+                this.Agreements = JsonConvert.DeserializeObject<List<Agreement>>(jsonContent);
+                Console.WriteLine($"Succesfully read from: {FilePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error has occurd: {ex.Message}");
+            }
+        }
+    }
+}
