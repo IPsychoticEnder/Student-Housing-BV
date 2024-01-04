0eb4bf6d83cc4f749f8049fbc2fc4de9b0cc148a
IpsychoticEnder <aaron.boekhoudt@gmnail.com>
2024-01-04 19:28:28 +0100

    Tasks and handler implemented

---
+++ Student Housing BV/Classes/Tasks/HandleTasks.cs
@@ -0,0 +1,57 @@
+﻿
+
+using Newtonsoft.Json;
+using Student_Housing_BV.Classes.Agreements;
+
+namespace Student_Housing_BV.Classes.Tasks
+{
+    public class HandleTasks
+    {
+        public List<Task> Tasks {  get; private set; }
+        public string FilePath { get; private set; }
+
+
+        public HandleTasks() 
+        {
+            Tasks = new List<Task>();
+            string relativeFilepath = "\\Data\\Json\\Tasks.json";
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            while (!currentDirectory.EndsWith("\\Student Housing BV"))
+            {
+                currentDirectory = Directory.GetParent(currentDirectory).FullName;
+            }
+            FilePath = $"{currentDirectory}{relativeFilepath}";
+
+            ReadFromJson();
+        }
+
+        public void ReadFromJson()
+        {
+            try
+            {
+                string jsonContent = File.ReadAllText(FilePath);
+
+                Tasks = JsonConvert.DeserializeObject<List<Task>>(jsonContent);
+                Console.WriteLine($"Succesfully read from: {FilePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error has occurd: {ex.Message}");
+            }
+        }
+
+        public void WriteToJson()
+        {
+            try
+            {
+                string jsonContent = JsonConvert.SerializeObject(Tasks, Formatting.Indented);
+                File.WriteAllText(FilePath, jsonContent);
+                Console.WriteLine($"Data succesfully written to: {FilePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error has occurd: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Student Housing BV/Classes/Tasks/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Housing_BV.Classes.Tasks
+{
+    public class Task
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Task(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
